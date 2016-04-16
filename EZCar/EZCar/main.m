ca9176184fce0c09e4ed8b65fbda4e8d4a9a8806
欧阳 <747377294@qq.com>
2016-04-16 14:21:42 +0800

    新建初始工程

---
+++ EZCar/EZCar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EZCar
+//
+//  Created by 欧阳 on 16/4/16.
+//  Copyright © 2016年 EZone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
