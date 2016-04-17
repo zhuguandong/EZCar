8e94d63feb6e82192d4f4d0d56dfd4396d7a2743
朱观东 <376058691@qq.com>
2016-04-17 20:05:23 +0800

    no message

---
+++ EZCar/EZCar/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  EZCar
+//
+//  Created by 欧阳 on 16/4/17.
+//  Copyright © 2016年 EZone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ EZCar/EZCar/FirstViewController.m
@@ -0,0 +1,65 @@
+//
+//  FirstViewController.m
+//  EZCar
+//
+//  Created by 欧阳 on 16/4/17.
+//  Copyright © 2016年 EZone. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //加上 搜索栏
+    UIView *titleView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 35)];//allocate titleView
+    UIColor *color =  self.navigationController.navigationBar.barTintColor;
+    
+    [titleView setBackgroundColor:color];
+    
+    UISearchBar *searchBar = [[UISearchBar alloc] init];
+    
+    
+    searchBar.delegate = self;
+    searchBar.frame = CGRectMake(0, 0, 200, 35);
+    searchBar.backgroundColor = color;
+    searchBar.layer.cornerRadius = 18;
+    searchBar.layer.masksToBounds = YES;
+    [searchBar.layer setBorderWidth:8];
+    [searchBar.layer setBorderColor:[UIColor whiteColor].CGColor];  //设置边框为白色
+    
+    searchBar.placeholder = @"宝马4S店";
+    [titleView addSubview:searchBar];
+    
+    //Set to titleView
+    [self.navigationItem.titleView sizeToFit];
+    self.navigationItem.titleView = titleView;
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+    
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+
+@end
